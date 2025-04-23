01d6070f0be27f7a219d520545aa8f9c9eaed214
Artur Gentil Silva Pinto <arturpinto2005@gmail.com>
2025-04-23 09:34:30 +0100

    .

--- routes/adminRoutes/categories.js
+++ routes/adminRoutes/categories.js
@@ -1,23 +1,49 @@
 const express = require("express");
 const router = express.Router();
 
-const authTokenMiddleware = require("../../Middleware/AuthTokenMiddleware.js");
-const typeUserMiddleware = require("../../Middleware/TypeUserMiddleware.js");
+//Models
 const categoriesController = require("../../Controllers/ControllersAdmin/CategoriesController.js");
-//Meter como na admin
 
-/*Routers para as categorias */
-router.get("/", authTokenMiddleware.authenticateToken, typeUserMiddleware.isAdmin, categoriesController.homePage);
+//Autentificação de token e de tipo de utilizador
+const {authenticateToken} = require("../Middleware/AuthTokenMiddleware.js");
+const {isAdmin} = require("../Middleware/TypeUserMiddleware.js");
 
-router.get("/createCategory", authTokenMiddleware.authenticateToken, typeUserMiddleware.isAdmin, categoriesController.createCategory);
+/**
+* Rota utilizada para aplicar os seguintes middleware as restantes rotas
+*/
+router.use(authenticateToken, isAdmin);
 
-router.post("/saveCategory", authTokenMiddleware.authenticateToken, typeUserMiddleware.isAdmin, categoriesController.saveCategory);
+/**
+ * Rota que carrega a pagina, com uma lista de todas as categorias
+ */
+router.get("/", categoriesController.homePage);
 
-router.get("/editCategory/:categoryId", authTokenMiddleware.authenticateToken, typeUserMiddleware.isAdmin, categoriesController.editCategory);
+/**
+ * Rota que carrega a pagina para criar uma categoria
+ */
+router.get("/createCategory", categoriesController.createCategory);
 
-router.post("/updatCategory/:categoryId", authTokenMiddleware.authenticateToken, typeUserMiddleware.isAdmin, categoriesController.updatCategory);
+/**
+ * Rota que guarda uma categoria, criada na página de criação de uma categoria
+ */
+router.post("/saveCategory", categoriesController.saveCategory);
 
-//Posso depois alterar para um delete
-router.post("/deletetCategory/:categoryId", authTokenMiddleware.authenticateToken, typeUserMiddleware.isAdmin, categoriesController.deleteCategory);
+/** 
+ * Rota que carrega a pagina para editar uma categoria
+ * Esta rota tem como paramentro o categoryId, que corresponde ao ID da categoria, a ser atualizada
+*/
+router.get("/editCategory/:categoryId", categoriesController.editCategory);
+
+/**
+ * Rota que faz o update dos dados de uma categoria
+ * Esta rota tem como paramentro o categoryId, que corresponde ao ID da categoria, a ser atualizada
+ */
+router.post("/updatCategory/:categoryId", categoriesController.updatCategory);
+
+/**
+ * Rota que faz o delete de uma categoria especifica
+ * Esta rota tem como paramentro o categoryId, que corresponde ao ID da categoria, a ser atualizada
+ */
+router.post("/deletetCategory/:categoryId", categoriesController.deleteCategory);
 
 module.exports = router;
